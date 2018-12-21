3acf3ebfd34065bfade49aab98709007129c7351
Cleber Campomori <cleber.campomori@gmail.com>
2018-12-21 13:39:01 -0200

    Aula 2.2: Gerenciamento básico de memória na JVM: Value-Types e Reference-Types

---
+++ tw-estrutura-dados/src/br/com/treinaweb/estruturadados/main/Main.java
@@ -0,0 +1,49 @@
+package br.com.treinaweb.estruturadados.main;
+
+import java.util.Scanner;
+
+import br.com.treinaweb.estruturadados.modelos.Pessoa;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Digite a opção desejada: ");
+		System.out.println("1. Gerenciamento de memória");
+		System.out.println("2. Vetores");
+		System.out.println("3. Lista ligada");
+		System.out.println("4. Lista duplamente ligada");
+		System.out.println("5. Pilha");
+		System.out.println("6. Fila");
+		Scanner scanner = new Scanner(System.in);
+		int opcao = scanner.nextInt();
+		switch (opcao) {
+		case 1:
+			fazerGerenciamentoMemoria();
+			break;
+		}
+		scanner.close();
+	}
+
+	private static void fazerGerenciamentoMemoria() {
+		int a = 3;
+		System.out.println(a);
+		int b = a;
+		System.out.println(b);
+		b = 2;
+		System.out.println("---------------------");
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(a == b);
+		System.out.println("*********************************");
+		Pessoa p1 = new Pessoa(1, "TreinaWeb");
+		System.out.println(p1.toString());
+		Pessoa p2 = new Pessoa(1, "TreinaWeb");
+		System.out.println(p2.toString());
+		System.out.println("---------------------");
+		p2.setNome("TreinaWeb Modificado");
+		System.out.println(p1.toString());
+		System.out.println(p2.toString());
+		System.out.println(p1 == p2);
+	}
+
+}
