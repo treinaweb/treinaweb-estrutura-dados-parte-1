5f8da068c39b8c73e9b8d58ed637fd2cf72673ea
Cleber Campomori <cleber.campomori@gmail.com>
2018-12-24 00:15:30 -0200

    3.5: Estruturas de Dados - vetores: criando a funcionalidade de inserção

--- tw-estrutura-dados/src/br/com/treinaweb/estruturadados/vetores/Vetor.java
+++ tw-estrutura-dados/src/br/com/treinaweb/estruturadados/vetores/Vetor.java
@@ -3,12 +3,27 @@ package br.com.treinaweb.estruturadados.vetores;
 public class Vetor<T> {
 
 	private Object[] elementos;
+	private int posicao;
 
 	public Vetor(int capacidade) {
 		this.elementos = new Object[capacidade];
+		this.posicao = 0;
+	}
+
+	public Vetor() {
+		this.elementos = new Object[3];
+		this.posicao = 0;
+	}
+
+	public void inserir(T elemento) {
+		this.elementos[this.posicao] = elemento;
+		this.posicao++;
 	}
 
 	public void inserirEm(int posicao, T elemento) {
+		if (posicao < this.elementos.length) {
+			throw new IllegalArgumentException(String.format("A posição é inválida [%d]", posicao));
+		}
 		this.elementos[posicao] = elemento;
 	}
 
