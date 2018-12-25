b49b26970a4138a227f169aad584e1bbb7451abd
Cleber Campomori <cleber.campomori@gmail.com>
2018-12-25 21:03:07 -0200

    Aula 7.3: Estruturas de Dados - filas: implementando as funcionalidades de enfileiramento, desenfileiramento e verificação de presença de nós

--- tw-estrutura-dados/src/br/com/treinaweb/estruturadados/filas/Fila.java
+++ tw-estrutura-dados/src/br/com/treinaweb/estruturadados/filas/Fila.java
@@ -7,19 +7,24 @@ public class Fila<T> {
 	private ListaLigada<T> elementos;
 
 	public Fila() {
-		this.elementos = new ListaLigada();
+		this.elementos = new ListaLigada<T>();
 	}
 
 	public void enfileirar(T elemento) {
-
+		this.elementos.inserir(elemento);
 	}
 
 	public T desenfileirar() {
-
+		if (estaVazia()) {
+			return null;
+		}
+		T resultado = this.elementos.recuperar(0);
+		this.elementos.remover(0);
+		return resultado;
 	}
 
 	public boolean estaVazia() {
-
+		return this.elementos.estaVazia();
 	}
 
 }
