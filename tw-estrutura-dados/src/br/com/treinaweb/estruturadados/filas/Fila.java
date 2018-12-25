7f947b7ec043acc8e77531a5277dd0beae7ac8b6
Cleber Campomori <cleber.campomori@gmail.com>
2018-12-25 21:00:57 -0200

    Aula 7.2: Estruturas de Dados - filas: definindo as funcionalidades a serem implementadas

---
+++ tw-estrutura-dados/src/br/com/treinaweb/estruturadados/filas/Fila.java
@@ -0,0 +1,25 @@
+package br.com.treinaweb.estruturadados.filas;
+
+import br.com.treinaweb.estruturadados.listasligadas.ListaLigada;
+
+public class Fila<T> {
+
+	private ListaLigada<T> elementos;
+
+	public Fila() {
+		this.elementos = new ListaLigada();
+	}
+
+	public void enfileirar(T elemento) {
+
+	}
+
+	public T desenfileirar() {
+
+	}
+
+	public boolean estaVazia() {
+
+	}
+
+}
